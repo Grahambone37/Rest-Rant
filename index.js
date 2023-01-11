54cc4fce7ba528c60f006d017e0db39c97d56bc1
Grahambone37 <electricgrunt37@gmail.com>
2023-01-10 17:11:45 -0800

    added 404 route

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 
 
 app.get('*', (req, res) => {
-    res.send('Page Not Found')
+    res.status(404).send(`<h1>404 Page</h1>`)
 })
 
 app.listen(process.env.PORT, () => {
