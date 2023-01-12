91bf373119f4a8ef18e40ed7156e0eb8bc17322a
Grahambone37 <electricgrunt37@gmail.com>
2023-01-11 21:44:07 -0800

    made home page stub in views

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('The Home Page')
+    res.render('home')
 })
-
 
 app.get('*', (req, res) => {
     res.status(404).send(`<h1>404 Page</h1>`)
@@ -17,8 +19,8 @@ app.get('*', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log("It's working")
 })
+  
 
 
 
-
-
+ 
