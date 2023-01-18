79843eb558a44f58579d1597a4da0776d8dadbbf
Grahambone37 <electricgrunt37@gmail.com>
2023-01-17 19:04:16 -0800

    getting the show page spruced up

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,24 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.place.name}</h1>
+                <div className="row">
+                    <div className='pic'>
+                        <img src={data.place.pic} alt="Rest-Rant pic"></img>
+                    </div>
+                    <div className='description'>
+                        <h2>Rating</h2>
+                        <h4>currently unrated</h4>
+                        <h2>Description</h2>
+                        <h4>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h4>
+                    </div>
+                </div>
+                <hr></hr>
+                
             </main>
         </Def>
     )
