1cc78ecb53c81d2630bb0a53d7503bf308492be9
Grahambone37 <electricgrunt37@gmail.com>
2023-01-26 14:45:36 -0800

    fixed default pic

--- models/places-array.js
+++ models/places-array.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 
 const placeSchema = new Schema({
     name: { type: String, required: true },
-    pic: { type: String, default: "http://placekitten.com/800/1200" },
+    pic: { type: String, default: "/images/default-food.jpg" },
     cuisines: { type: String, required: true },
     city: { type: String, default: "Anytown" },
     state: { type: String, default: "USA" },
